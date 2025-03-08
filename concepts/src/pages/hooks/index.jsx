8d11f132246773bde32819129c881b65ca0fd9c2
Raja <orajacse@gmail.com>
2025-03-08 20:16:29 +0530

    Built-in React Hooks

--- concepts/src/pages/hooks/index.jsx
+++ concepts/src/pages/hooks/index.jsx
@@ -4,8 +4,96 @@ import { Link } from "react-router-dom";
 export const Hooks = () => {
   return (
     <>
-      <h4>Hooks !</h4>
+      <h4>Built-in React Hooks</h4>
       <ul>
+        <li>
+          <h5>State Hooks</h5>
+          <ul>
+            <li>
+              <Link>useState</Link>
+            </li>
+            <li>
+              <Link>useReducer</Link>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h5>Context Hooks</h5>
+          <ul>
+            <li>
+              <Link to="/usecontext">! Use Context</Link>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h5>Ref Hooks</h5>
+          <ul>
+            <li>
+              <Link>useRef</Link>
+            </li>
+            <li>
+              <Link>useImperativeHandle</Link>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h5>Effect Hooks</h5>
+          <ul>
+            <li>
+              <Link>useEffect</Link>
+            </li>
+            <li>
+              <Link>useLayoutEffect</Link>
+            </li>
+            <li>
+              <Link>useInsertionEffect</Link>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h5>Performance Hooks</h5>
+          <ul>
+            <li>
+              <Link to="/usememo">! Use Memo</Link>
+            </li>
+            <li>
+              <Link to="/usecallback">! Use Callback</Link>
+            </li>
+            <li>
+              <Link to="/usetransition">! Use Transition</Link>
+            </li>
+            <li>
+              <Link>useDeferredValue</Link>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h5>Other Hooks</h5>
+          <ul>
+            <li>
+              <Link>useDebugValue</Link>
+            </li>
+            <li>
+              <Link>useId</Link>
+            </li>
+            <li>
+              <Link>useSyncExternalStore</Link>
+            </li>
+            <li>
+              <Link>useActionState</Link>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h5>Custom Hooks</h5>
+          <ul>
+            <li>
+              <Link to="/customhook">! Custom Hook</Link>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      {/* <ul>
         <li>
           <Link to="/usememo">Use Memo</Link>
         </li>
@@ -21,7 +109,7 @@ export const Hooks = () => {
         <li>
           <Link to="/customhook">Custom Hook</Link>
         </li>
-      </ul>
+      </ul> */}
     </>
   );
 };
