9f8c0cc222c560d1f408362b618694b069b156f8
Raja <orajacse@gmail.com>
2025-03-08 12:29:42 +0530

    useCallback Example

--- concepts/src/pages/hooks/useCallback/index.jsx
+++ concepts/src/pages/hooks/useCallback/index.jsx
@@ -1,18 +1,37 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import BackToHome from "../../../components/backToHome";
 import Product from "./product";
 import "./index.css";
 
 const UseCallBack = () => {
-  const [prod] = useState(["Prod 1", "Prod 2", "Prod 3"]);
+  console.log("useCallBack component rendered");
+
+  const [prod] = useState(["Prod 1", "Prod 2"]);
+  const [count, setCount] = useState(0);
+  const [cart, setCart] = useState(0);
+
+  const addToCart = useCallback(() => {
+    setCart(cart + 1);
+  }, [cart]);
+
+  // Non useCallback
+  //   const addToCart = () => {
+  //     setCart(cart + 1);
+  //   };
+
   return (
     <>
       <BackToHome />
       <h4>useCallback</h4>
-      <div className="product-wrap">
-        {prod.map((val, ind) => {
-          return <Product name={val} key={ind} />;
-        })}
+      <div className="useCallBack">
+        <h3>Count: {count}</h3>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <h3>Cart: {cart}</h3>
+        <div className="product-wrap">
+          {prod.map((val, ind) => {
+            return <Product name={val} key={ind} addToCart={addToCart} />;
+          })}
+        </div>
       </div>
     </>
   );
--- concepts/src/pages/hooks/useCallback/product.jsx
+++ concepts/src/pages/hooks/useCallback/product.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import { memo } from "react";
 
-const Product = ({name}) => {
+const Product = memo(({ name, addToCart }) => {
+  console.log(`${name} Prod component re-renderd`);
   return (
-    <div className='product'>
-        <h1>{name}</h1>
+    <div className="product">
+      <h1>{name}</h1>
+      <button onClick={addToCart}>Add to Cart</button>
     </div>
-  )
-}
+  );
+});
 
-export default Product
+export default Product;
